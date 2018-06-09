187cc553cb59bb265b14ef1016f87e92f2f10ed2
Claudia-PC\Claudia <claudia.mvasile@gmail.com>
2018-06-09 12:58:03 +0300

    Add project files.

---
+++ E-LearningApplication-Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_LearningApplication_Final.Startup))]
+namespace E_LearningApplication_Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
